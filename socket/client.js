c3952ebfae1c7c4b5cf10153e66d54b296bd2ea1
hanjixin <764671440@qq.com>
2020-02-04 16:50:21 +0800

    加入通知数量

--- socket/client.js
+++ socket/client.js
@@ -4,3 +4,6 @@ socket.connect(8080,'127.0.0.1')
 socket.write('hello','utf-8', function(e) {
     console.log(e, '发送成功')
 });
+socket.on('data',function(e) {
+    console.log('接收数据:' + e)
+})
--- socket/server.js
+++ socket/server.js
@@ -2,7 +2,8 @@ let net = require('net');
 let util = require('util');
 let server = net.createServer(function(socket){
     server.getConnections((err,count)=>{
-        server.maxConnections = 1;
+        server.maxConnections = 3;
+        socket.write(`weclome,there is ${count} users now,please input your username\r\n`);
         console.log('最大连接数量%d,当前连接数量%d',server.maxConnections,count); 
     });  
     socket.on('data', function(e) {
